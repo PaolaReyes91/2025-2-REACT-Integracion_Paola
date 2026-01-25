966673429312544f7a3e66aed47c7958dc52907e
PaolaReyes91 <paoreyesmacias@gmail.com>
2026-01-25 15:14:49 -0600

    Add files via upload

--- 02.CodeChallenge/Reto03/Login.jsx
+++ 02.CodeChallenge/Reto03/Login.jsx
@@ -1,1 +1,49 @@
+import { useState } from 'react';
 
+export default function Login() {
+    const [password, setPassword] = useState("");
+    const SECRET_KEY = "reto03"
+    const [isAuth, setIsAuth] = useState(() => {
+        return localStorage.getItem("user_auth") === "true";
+    });
+
+
+  const handleLogin = () => {
+    if (password === SECRET_KEY){
+        setIsAuth(true);
+        localStorage.setItem("user_auth", "true");
+    } else {
+        alert ("Contraseaña Incorrecta"); 
+        setPassword ("");   
+    }
+  };
+
+  const handleLogout = () => {
+    setIsAuth(false);
+    localStorage.removeItem("user_auth");
+  };
+
+ if (isAuth)  {
+    return (
+      <div>
+        <h1>Bienvenido al club secreto ✨</h1>
+        {/* Contenido especial aquí */}
+        <button onClick={handleLogout}>Salir</button>
+      </div>
+    );
+}
+
+  return (
+    <div>
+      <h2>Acceso restringido</h2>
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Entrar</button>
+
+    </div>
+  );
+}
---
+++ 02.CodeChallenge/Reto03/README.md
@@ -0,0 +1,4 @@
+Escribe 2–3 frases en tu README: ¿qué diferencia viste entre estado volátil y localStorage?
+La diferencia entre estado volátil y localStorage, es que el estatus volátil es aquel que mientras no actualices el estado este permanece y el localStorage nos permite guardarlo.
+¿Qué pasó cuando recargaste sin el sello?
+Se marcaba contraseña erronea.
